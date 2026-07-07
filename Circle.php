9c6be9fcf9b80e90287b033f61650ab93a1839e0
shindeswati66130-rgb <shindeswati66130@gmail.com>
2026-07-07 13:32:07 +0530

    Create Circle.php

---
+++ Circle.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Area of Circle</title>
+  </head>
+  <body>
+    $r=5;
+    cont pi=3.14;
+    $area=pi*$r*$r;
+    echo"Radius of Circle=".$r."<br>";
+    echo"Area of Circle=".$area;
+    ?>
+  </body>
+</html>
